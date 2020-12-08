1062d63f0eca58c7e0eccf59601a4a94de32e6ef
Hanna Rosenfeld <thesoftestbrain@gmail.com>
2020-12-08 13:12:52 -0500

    Hotfix.

--- src/pages/email.js
+++ src/pages/email.js
@@ -4,17 +4,19 @@ import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div class="col-sm-5 col-md-5">
-      <div id="center-wide">
-        <div class="article_wrapper">
-          <h1>Contact Form</h1>
-          <form method="post" action="">
-            <button type="submit" class="btn btn-success">
-              Send Message
-            </button>
-          </form>
+    <Layout>
+      <div class="col-sm-5 col-md-5">
+        <div id="center-wide">
+          <div class="article_wrapper">
+            <h1>Contact Form</h1>
+            <form method="post" action="">
+              <button type="submit" class="btn btn-success">
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
