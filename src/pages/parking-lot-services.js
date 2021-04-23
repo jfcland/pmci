14f01c70a08babe2c6f28576854013c78119b5b5
hayduke <hayduke@badlands.local>
2021-04-23 15:00:26 -0400

    Cleanup main categories.

--- src/pages/parking-lot-services.js
+++ src/pages/parking-lot-services.js
@@ -2,6 +2,79 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-export default function Home() {
-  return <Layout></Layout>
+import blacktopSealing from "../static/img/blacktop-sealing.jpg"
+import crackRepair from "../static/img/crack-repair.jpg"
+import patchRepairs from "../static/img/patch-repairs.jpg"
+import trafficMarkings from "../static/img/traffic-markings.jpg"
+import constructionMan from "../static/img/construction-man.jpg"
+
+
+
+export default function ParkingLotServices() {
+  return (
+    <Layout>
+      <div class="row service">
+        <div class="pmci-favicon">
+          <h2>Parking Lot Services</h2>
+        </div>
+      </div>
+
+      <div class="row service">
+        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+          <a href="seal-coating/index.html">
+            <img src={blacktopSealing} />
+          </a>
+        </div>
+        <div class="col-sm-3 col-md-2 category-teaser">
+          <h3><a href="seal-coating/index.html">Seal Coating</a></h3>
+          <div class="teaser">Application of the best materials in the correct quantities with a warranty.</div>
+        </div>
+
+        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+          <a href="blacktop-repairs/index.html">
+            <img src={patchRepairs}/>
+          </a>
+        </div>
+        <div class="col-sm-3 col-md-2 category-teaser">
+          <h3><a href="blacktop-repairs/index.html">Blacktop Repairs</a></h3>
+          <div class="teaser">Repair of damaged blacktop with new blacktop and specialized materials.</div>
+        </div>
+      </div>
+
+      <div class="row service">
+        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+          <a href="crack-repair/index.html">
+            <img src={crackRepair} />
+          </a>
+        </div>
+        <div class="col-sm-3 col-md-2 category-teaser">
+          <h3><a href="crack-repair/index.html">Crack Repair</a></h3>
+          <div class="teaser">Repair of cracks using the best materials and methods available</div>
+        </div>
+
+        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+          <a href="pavement-markings/index.html">
+            <img src={trafficMarkings}/>
+          </a>
+        </div>
+        <div class="col-sm-3 col-md-2 category-teaser">
+          <h3><a href="pavement-markings/index.html">Pavement Markings</a></h3>
+          <div class="teaser">Painting or removal of parking stripes, traffic lines and symbols. Installation of signs, car stops, and guard rails.</div>
+        </div>
+
+      </div>
+      <div class="row service">
+        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+          <a href="pavement-consultation/index.html">
+            <img src={constructionMan} />
+          </a>
+        </div>
+        <div class="col-sm-3 col-md-2 category-teaser">
+          <h3><a href="pavement-consultation/index.html">Pavement Consultation</a></h3>
+          <div class="teaser">For new construction or existing blacktop.</div>
+        </div>
+
+      </div>
+    </Layout>
+  )
 }
--- src/pages/recreational-court-services.js
+++ src/pages/recreational-court-services.js
@@ -1,45 +1,143 @@
 import React from "react"
 import Layout from "../components/layout"
 
-import acc from "../static/img/acrylic-color-coatings.jpg"
-import pl from "../static/img/playing-lines.jpg"
+import acrylic from "../static/img/acrylic-color-coatings.jpg"
+import crackRepair from "../static/img/crack-repair-2.jpg"
+import equipmentInstallation from "../static/img/equipment-installation.jpg"
+import unicourt from "../static/img/unicourt.jpg"
+import patching from "../static/img/patching.jpg"
+import pavementConsultation from "../static/img/pavement-consultation.jpg"
+import playingLines from "../static/img/playing-lines.jpg"
+import specialtyCoatings from "../static/img/specialty-coatings.jpg"
 
-export default function Home() {
+
+export default function RecreationalCourtServices() {
   return (
     <Layout>
       <div class="row service">
-        <h2>Recreational Court Services</h2>
-        <div class="pmci-favicon"></div>
+        <div class="pmci-favicon">
+          <h2>Recreational Court Services</h2>
+        </div>
+
         <div class="row service">
-          <div class="col-sm-3 col-md-3 col-md-offset-1 category-img">
-            <a href="">
-              <h3>Acrylic Color Coatings</h3>
-              <img src={acc} alt="acrylic-color-coatings" />
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="acrylic/index.html">
+              <img src={acrylic} />
             </a>
-            <p>
-              Installation of outdoor and indoor color coat systems in a wide
-              variety of colors to new or existing recreational courts.
-            </p>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="acrylic/index.html">Acrylic Color Coatings</a></h3>
+            <div class="teaser">Installation of outdoor and indoor color coat systems in a wide variety of colors to new or existing recreational courts.
+            </div>
           </div>
 
-          <div class="col-sm-3 col-md-3 col-md-offset-1 category-img">
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="patching/index.html">
+              <img src={patching}/>
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="patching/index.html">Patching</a></h3>
+            <div class="teaser">Repair of damaged or sunken areas of recreational courts</div>
+          </div>
+        </div>
+
+        <div class="row service">
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
             <a href="">
               <h3>Playing Lines</h3>
-              <img src={pl} alt="playing lines" />
+              <img src={playingLines} alt="playing lines" />
             </a>
             <p>
               Layout and installation of regulation playing lines for all sports
             </p>
           </div>
-
           <div class="col-sm-3 col-md-2 category-teaser">
-            <h3>
-              <a href=""></a>
-            </h3>
-            <div class="teaser"></div>
+            <h3><a href="playing-lines/index.html">Playing Lines</a></h3>
+            <div class="teaser">Layout and installation of regulation playing lines for all sports</div>
+          </div>
+
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="crack-repair-2/index.html">
+              <img src={crackRepair} />
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="crack-repair-2/index.html">Crack Repair</a></h3>
+            <div class="teaser">Repair of cracks using the most up to date methods and materials</div>
+          </div>
+        </div>
+
+        <div class="row service">
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="equipment-installation/index.html">
+              <img src={equipmentInstallation} />
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="equipment-installation/index.html">Equipment Installation</a></h3>
+            <div class="teaser">Furnishing and installation of equipment for all recreational courts</div>
+          </div>
+
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="specialty-coatings/index.html">
+              <img src={specialtyCoatings} />
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="specialty-coatings/index.html">Specialty Coatings</a></h3>
+            <div class="teaser">Specialty coatings that include cushioned, street hockey and inline skating surfaces.</div>
+          </div>
+        </div>
+
+        <div class="row service">
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="pavement-consultation-2/index.html">
+              <img src={pavementConsultation} />
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="pavement-consultation-2/index.html">Pavement Consultation</a></h3>
+            <div class="teaser">Design and implement refurbishing of existing courts or building of new courts.</div>
+          </div>
+
+          <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
+            <a href="unicourt/index.html">
+              <img src={unicourt} />
+            </a>
+          </div>
+          <div class="col-sm-3 col-md-2 category-teaser">
+            <h3><a href="unicourt/index.html">UniCourt</a></h3>
+            <div class="teaser">One Court For All Sports Great Backyard Family Fun Custom design for your favorite sports. Many different court sizes, configurations &amp; colors to choose from.</div>
+          </div>
+        </div>
+
+      </div>
+
+      <div id="footer">
+        <div class="row">
+          <div class="inner">
+            <span class="phone"><a href="tel:16108376703"><span class="city">Lehigh Valley</span> <i class="fa fa-phone"></i> 610-837-6703</a></span>
+            <span class="phone"><a href="tel:15704215532"><span class="city">Poconos</span> <i class="fa fa-phone"></i> 570-421-5532</a></span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="outer">
+            Copyright &#169; 1978-2020 Pavement Maintenance Contractors Inc.
+            <span class="developed-by"><a href="http://nillab.com/" target="_blank" title="Developed by Nillab">Developed by nillab</a></span>
           </div>
         </div>
       </div>
+
+
+      <div class="col-sm-3 col-md-2 category-teaser">
+        <h3>
+          <a href=""></a>
+        </h3>
+        <div class="teaser"></div>
+      </div>
+
+
     </Layout>
   )
 }
