7f74ea9a29356989e153d9219b70af6184e887d5
hayduke <hayduke@badlands.local>
2021-04-23 16:21:02 -0400

    Enable contact form.

--- src/pages/email.js
+++ src/pages/email.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-export default function Contact extends React.Component {
+export default class Contact extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
