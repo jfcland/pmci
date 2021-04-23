066417c00df6f52d03ac65dffa63676bfa608aa7
hayduke <hayduke@badlands.local>
2021-04-23 16:04:59 -0400

    Enable contact form.

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-export default SuccessPage() {
+export default function SuccessPage() {
   return (
     <Layout>
       <h1>
