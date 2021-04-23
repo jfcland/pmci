0e2db59515b0cec902ff2d62b43df69a58623eaa
hayduke <hayduke@badlands.local>
2021-04-23 16:56:43 -0400

    Enable contact form.

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,10 +5,12 @@ import Layout from '../components/layout'
 export default function SuccessPage() {
   return (
     <Layout>
-      <h1>
-        <div id="success" />
-        Thank you for contacting us, we will reply asap.
-      </h1>
+      <div class="row">
+        <h1>
+          <div id="success" />
+          Thank you for contacting us, we will reply asap.
+        </h1>
+      </div>
     </Layout>
   )
 }
