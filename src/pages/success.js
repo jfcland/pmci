8c4fe82420fddbc6269543e7066efb1c132a2c0c
hayduke <hayduke@badlands.local>
2021-04-23 17:04:04 -0400

    Enable contact form.

--- src/pages/success.js
+++ src/pages/success.js
@@ -7,7 +7,7 @@ export default function SuccessPage() {
     <Layout>
       <div class="row">
         <h1>Thank you for contacting us.</h1>
-        <div id="success" />
+        <div id="success">
           A response will be sent to the email address you’ve indicated within 2 business days. If it’s urgent, we encourage you to call us a call. Have a wonderful day!
         </div>
       </div>
