6585ef17fba796697329c5f5ac80115097fcc293
hayduke <hayduke@badlands.local>
2021-04-23 15:44:04 -0400

    Style contact form.

--- src/pages/email.js
+++ src/pages/email.js
@@ -2,29 +2,36 @@ import React from "react"
 
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Email() {
   return (
     <Layout>
-      <div class="col-sm-5 col-md-5">
-        <div id="center-wide">
-          <div class="article_wrapper">
-            <h1>Contact Us</h1>
+      <div class="row">
+        <div class="col-sm-1 col-md-1"></div>
+        <div class="col-sm-4 col-md-4">
+          <div id="center-wide">
+            <div class="article_wrapper">
+              <h1>Contact Us</h1>
 
-            <form name="contact" method="POST" data-netlify="true">
-              <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-              </p>
-              <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-              </p>
-              <p>
-                <label>Message: <textarea name="message"></textarea></label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
-            </form>
+              <form name="contact" method="POST" data-netlify="true">
+                <div class="form-group">
+                  <label>Your Name:</label>
+                  <input type="text" class="form-control" name="name" />
+                </div>
 
+                <div class="form-group">
+                  <label>Your Email: </label>
+                  <input type="email" class="form-control" name="email" />
+                </div>
+
+                <div class="form-group">
+                  <label>Message: </label>
+                  <textarea name="message" class="form-control" rows="6"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-default">Send</button>
+              </form>
+
+            </div>
           </div>
         </div>
       </div>
