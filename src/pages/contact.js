882090af9515d69a78159dd69b4ba92f310724fd
Thomas Schreiber <rizumu@users.noreply.github.com>
2024-03-20 20:45:54 -0400

    Update contact info.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,52 +16,32 @@ export default function Contact() {
 
           <p>
             <strong>
-              Pavement Maintenance Contractors Inc.
-              <br />
-              1039 Trexlertown Road
-              <br />
-              PO Box 1039
-              <br />
-              Trexlertown, PA 18087
-              <br />
+              Pavement Maintenance Contractors Inc.<br />
+              1039 Trexlertown Road<br />
+              PO Box 1039<br />
+              Trexlertown, PA 18087<br />
             </strong>
           </p>
           <p>
-            <strong>Lehigh Valley:</strong>{" "}
-            <a href="tel:16108376703">610-837-6703</a>
-            <br />
-            <strong>Poconos:</strong> <a href="tel:15704215532">570-421-5532</a>
-            <br />
-            <strong>Fax:</strong> 610-837-8554
-            <br />
+            <strong>Lehigh Valley: </strong><a href="tel:16108376703">610-837-6703</a><br />
+            <strong>Poconos: </strong><a href="tel:15704215532">570-421-5532</a><br />
           </p>
           <p>
-            <strong>President:</strong> Joe Ciccone
-            <br />
-            <strong>Phone:</strong>{" "}
-            <a href="tel:16108376703">610-837-6703 EXT. 301</a>
-            <br />
-            <strong>Email:</strong>{" "}
+            <strong>President: </strong>Joe Ciccone<br />
+            <strong>Phone: </strong><a href="tel:16108376703">610-837-6703 EXT. 301</a><br />
+            <strong>Email: </strong>
             <a href="mailto:jciccone:jciccone@pavementcontractors.com">
               jciccone@pavementcontractors.com
-            </a>
-            <br />
+            </a><br />
           </p>
           <p>
-            <strong>Project Manager:</strong> Patrick Gargone
-            <br />
-            <strong>Phone:</strong>{" "}
-            <a href="tel:16108376703">610-837-6703 EXT. 305</a>
-            <br />
-            <strong>Facsimile:</strong> <a href="tel:14842399005">610-837-8554</a>
-            <br />
-            <strong>Cell:</strong> <a href="tel:14842399005">484-239-9005</a>
-            <br />
-            <strong>Email:</strong>{" "}
-            <a href="mailto:pgargone@pavementcontractors.com">
-              pgargone@pavementcontractors.com
-            </a>
-            <br />
+            <strong>Project Manager:</strong> Ron Brand<br />
+            <strong>Phone: </strong><a href="tel:16108376703">610-837-6703 EXT. 305</a><br />
+            <strong>Cell: </strong><a href="tel:14842399006">484-239-9006</a><br />
+            <strong>Email: </strong>
+            <a href="mailto:rbrand@pavementcontractors.com">
+              rbrand@pavementcontractors.com
+            </a><br />
           </p>
           <p>
             <a href="/">
@@ -70,8 +50,7 @@ export default function Contact() {
           </p>
         </div>
         <div class="col-sm-6 col-md-6 right">
-          <h2>Primary Service Area</h2>
-          <br />
+          <h2>Primary Service Area</h2><br />
           <img src={service_area} />
         </div>
       </div>
