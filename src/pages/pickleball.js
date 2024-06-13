413c83640aff0b264caeebad99a5faa24610fe2e
Thomas Schreiber <rizumu@users.noreply.github.com>
2024-06-13 17:13:53 -0400

    Better pickleball copy.

--- src/pages/pickleball.js
+++ src/pages/pickleball.js
@@ -1,152 +1,48 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
 
-import acrylic from "../static/img/acrylic-color-coatings.jpg"
-import crackRepair from "../static/img/crack-repair-2.jpg"
-import equipmentInstallation from "../static/img/equipment-installation.jpg"
-import pickleball from "../static/img/pickleball.jpg"
-import patching from "../static/img/patching.jpg"
-import pavementConsultation from "../static/img/pavement-consultation.jpg"
-import playingLines from "../static/img/playing-lines.jpg"
-import specialtyCoatings from "../static/img/specialty-coatings.jpg"
+import Layout from '../components/layout'
+
+import img1 from "../static/img/secondary/equipment-installation_01.jpg"
+import img2 from "../static/img/secondary/equipment-installation_02.jpg"
 
 
-export default function RecreationalCourtServices() {
+export default function SuccessPage() {
   return (
     <Layout>
       <div class="m-1">
-      <br />
-
-      <div class="row service">
-        <div class="pmci-favicon">
-          <h2>Recreational Court Services</h2>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-1" id="service-name">
+          <h1>Equipment Installation</h1>
         </div>
       </div>
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-1">
 
-      <br />
-
-      <div class="row service">
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/acrylic/">
-            <img src={acrylic} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/acrylic-color-coatings/">Acrylic Color Coatings</a></h3>
-          <div class="teaser">Installation of outdoor and indoor color coat systems in a wide variety of colors to new or existing recreational courts.
+          <div class="service-photo">
+            <div><img src={img1} alt=""/></div>
+            <div class="caption"></div>
           </div>
-        </div>
 
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/patching/">
-            <img src={patching} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/patching/">Patching</a></h3>
-          <div class="teaser">Repair of damaged or sunken areas of recreational courts.</div>
-        </div>
+          <div class="service-photo">
+            <div><img src={img2} alt=""/></div>
+            <div class="caption"></div>
+          </div>
 
+        </div>
       </div>
-
-      <div class="row service">
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/pickleball/">
-            <img src={pickleball} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/pickleball/">Pickleball</a></h3>
-          <div class="teaser">We Specalize in Pickleball courts.</div>
-        </div>
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/playing-lines/">
-            <h3>Playing Lines</h3>
-            <img src={playingLines} alt="playing lines" />
-          </a>
-          <p>
-            Layout and installation of regulation playing lines for all sports.
+      <div class="row">
+        <div class="col-sm-7 col-sm-offset-2 col-md-7 col-md-offset-1" id="service-description">
+          <p><font face="Arial">Pavement
+            Maintenance Contractors, Inc. installs and replaces tennis and
+            basketball equipment and structures. High strength concrete that is
+            correctly sized and shaped will form the foundation for tennis court net
+            posts, tennis court net center tie down anchors and basketball posts
+            and backboards. A wide selection of equipment is available to meet your
+            needs. Your experienced PMCI representative will provide options and
+            designs to meet your needs.</font>
           </p>
         </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/playing-lines/">Playing Lines</a></h3>
-          <div class="teaser">Layout and installation of regulation playing lines for all sports.</div>
-        </div>
       </div>
-
-      <div class="row service">
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/crack-repair-2/">
-            <img src={crackRepair} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/crack-repair-2/">Crack Repair</a></h3>
-          <div class="teaser">Repair of cracks using the most up to date methods and materials.</div>
-        </div>
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/equipment-installation/">
-            <img src={equipmentInstallation} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/equipment-installation/">Equipment Installation</a></h3>
-          <div class="teaser">Furnishing and installation of equipment for all recreational courts.</div>
-        </div>
-
-      </div>
-
-      <div class="row service">
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/specialty-coatings/">
-            <img src={specialtyCoatings} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/specialty-coatings/">Specialty Coatings</a></h3>
-          <div class="teaser">Specialty coatings that include cushioned, street hockey and inline skating surfaces.</div>
-        </div>
-
-        <div class="col-sm-2 col-md-2 col-md-offset-1 category-img">
-          <a href="/pavement-consultation-2/">
-            <img src={pavementConsultation} alt=""/>
-          </a>
-        </div>
-        <div class="col-sm-3 col-md-2 category-teaser">
-          <h3><a href="/pavement-consultation-2/">Pavement Consultation</a></h3>
-          <div class="teaser">Design and implement refurbishing of existing courts or building of new courts.</div>
-        </div>
-
-      </div>
-
-      <div id="footer">
-        <div class="row">
-          <div class="inner">
-            <span class="phone"><a href="tel:16108376703"><span class="city">Lehigh Valley</span> <i class="fa fa-phone"></i> 610-837-6703</a></span>
-            <span class="phone"><a href="tel:15704215532"><span class="city">Poconos</span> <i class="fa fa-phone"></i> 570-421-5532</a></span>
-          </div>
-        </div>
-        <div class="row">
-          <div class="outer">
-            Copyright &#169; 1978-2024 Pavement Maintenance Contractors Inc.
-            <span class="developed-by"><a href="http://nillab.com/" target="_blank" title="Developed by Nillab">Developed by nillab</a></span>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="col-sm-3 col-md-2 category-teaser">
-        <h3>
-          <a href=""></a>
-        </h3>
-        <div class="teaser"></div>
-      </div>
-
       </div>
     </Layout>
   )
