8a18dacb9cc8879049467a54e06fe697ee915abf
Thomas Schreiber <rizumu@users.noreply.github.com>
2024-06-26 17:09:34 -0400

    NotFound and Error pages.

---
+++ src/pages/404.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <div className="container mx-auto w-2/5 m-[25px] px-[23px] py-[15px]">
+        <div className="text-3xl text-green-700 font-bold pb-4">
+          Page not found
+        </div>
+        <div className="pb-2">
+          Oops the page you have been looking for has been removed or relocated.
+        </div>
+        <div className="pb-2">
+          <Link to="/" className="text-sky-600 hover:text-green-400">
+            Go Back
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
